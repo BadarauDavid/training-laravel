fc9b43eb1df71e1d937102b4ad9a4c7eea48a86d
Cosmin Morari <cosmin.morari@5psolutions.eu>
2024-04-18 13:57:12 +0300

    #11 Migrations, eloquent models, relations https://pm.5psolutions.net/public/index.php?path_info=projects/training-david-badarau/tasks/11 make product factory and seed db

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence(),
+            'description' => $this->faker->paragraph(),
+            'price' => $this->faker->randomFloat(2, 10),
+            'img_link' => uniqid() . '.jpg',
+
+        ];
+    }
+}
